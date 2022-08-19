570c297ef4dbbb03a6e1cd77d4a7649d63dbc6bc
Jay Ring-Sakabe <jrins@Jays-MacBook-Pro.local>
2022-08-19 17:19:38 -0600

    made an add engineer function with its own questionnaire

--- script.js
+++ script.js
@@ -1,71 +1,141 @@
+const newmember = require('./newmember.js')
 //1 create  test for function that creates an individual team member object that contains their name, position, id number, email, and github
 //2 create test for function that prompts user using inquirer to answer questionaire for assembling information about their team.
 //3 create test for a function that then takes the responses from the inquirer and then displays them in a brand new HTML document.
 //4 Use bootstrap so the html document has uniform styling.
-const inquirer = require ('inquirer');
-const fs = require ('fs');
-// const newmember = require ('./newmember.js')
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-function Newmember (name, position, id, office, email, github){
-    this.name = name;
-    this.position = position;
-    this.id = id;
-    this.office = office;
-    this.email = email;
-    this.github = github;
-    this.member = () => {
-        console.log([this.name])
-    }
+
+const addEngineer = () => {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: "What is this engineer's full name?",
+            },
+            // {
+            //     type: 'input',
+            //     message: 'Record their identification number.',
+            //     name: 'id',
+            // },
+            // {
+            //     type: 'input',
+            //     message: 'Write their office number.',
+            //     name: 'office',
+            // },
+            // {
+            //     type: 'input',
+            //     message: 'Write their email.',
+            //     name: 'email',
+            // },
+            // {
+            //     type: 'input',
+            //     message: 'Write their identification number.',
+            //     name: 'github',
+            // },
+        ])
+        .then((engineerData) => {
+            const renEngDat = JSON.stringify(engineerData)
+            console.log(`Here's the information about the engineer:${renEngDat}`)
+            addNewOrExit()
+        })
 }
 
-inquirer
-    .prompt([
-        {
-            type: 'input',
-            name: 'name',
-            message: "What is your team's manager's full name?",
-        },
-        // {
-        //     type: 'input',
-        //     message: 'Record their identification number.',
-        //     name: 'id',
-        // },
-        // {
-        //     type: 'input',
-        //     message: 'Write their office number.',
-        //     name: 'office',
-        // },
-        // {
-        //     type: 'input',
-        //     message: 'Write their email.',
-        //     name: 'email',
-        // },
-        // {
-        //     type: 'input',
-        //     message: 'Write their identification number.',
-        //     name: 'github',
-        // },
-    
-    ])
+function addIntern() {
+    // //Todo, modify:
+    // type: 'input',
+    // name: 'name',
+    // message: "What is your team's manager's full name?",
+    // },
+    // {
+    //     type: 'input',
+    //     message: 'Record their identification number.',
+    //     name: 'id',
+    // },
+    // {
+    //     type: 'input',
+    //     message: 'Write their office number.',
+    //     name: 'office',
+    // },
+    // {
+    //     type: 'input',
+    //     message: 'Write their email.',
+    //     name: 'email',
+    // },
+    // {
+    //     type: 'input',
+    //     message: 'Write their identification number.',
+    //     name: 'github',
+    // },
 
-    .then ((data) =>{
-        const managerData = new Newmember(data.name, "Manager", data.id, data.office, data.email, data.github)
-        console.log(data)
-        inquirer.prompt([
+}
+
+const addNewOrExit = () => {
+    inquirer
+        .prompt([
             {
                 type: 'list',
                 message: 'What would you like to do next?',
                 name: "nextMember1",
-                choices: ["Add an engineer","Add an intern", "Exit"],
-            },
+                choices: ["Add an engineer", "Add an intern", "Exit"],
+            }
+        ])
+
+        .then((data) => {
+            if (data.nextMember1 === "Exit") {
+                //exit the inquirer and build the team
+                buildTeam(data);
+                console.log(data)
+            }
+            if (data.nextMember1 === "Add an engineer") {
+                console.log("engineer")
+                addEngineer()
+            }
+            if (data.nextMember1 === "Add an intern") {
+                addIntern()
+                console.log("intern")
+            }
+        })
+    }
+
+const init = () => {
+    inquirer
+        .prompt([
             {
                 type: 'input',
                 name: 'name',
                 message: "What is your team's manager's full name?",
             },
+            // {
+            //     type: 'input',
+            //     message: 'Record their identification number.',
+            //     name: 'id',
+            // },
+            // {
+            //     type: 'input',
+            //     message: 'Write their office number.',
+            //     name: 'office',
+            // },
+            // {
+            //     type: 'input',
+            //     message: 'Write their email.',
+            //     name: 'email',
+            // },
+            // {
+            //     type: 'input',
+            //     message: 'Write their identification number.',
+            //     name: 'github',
+            // },
+
         ])
 
-        .then ((data2) =>{
-                console.log(data2)
-    })
-    })
+        .then((data) => {
+            const managerData = new newmember.Newmember(data.name, "Manager", data.id, data.office, data.email, data.github)
+            console.log(data)
+            addNewOrExit()
+        })
+    }
+
+    init();

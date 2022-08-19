2b09b5885896c29828ba3e8412838b2b20b12136
Jay Ring-Sakabe <jrins@Jays-MacBook-Pro.local>
2022-08-19 17:53:38 -0600

    completed prompts and prompt chains

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
-const newmember = require('./newmember.js')
+// const newmember = require('./newmember.js')
 //1 create  test for function that creates an individual team member object that contains their name, position, id number, email, and github
 //2 create test for function that prompts user using inquirer to answer questionaire for assembling information about their team.
 //3 create test for a function that then takes the responses from the inquirer and then displays them in a brand new HTML document.
 //4 Use bootstrap so the html document has uniform styling.
 const inquirer = require('inquirer');
 const fs = require('fs');
+const teamInfo = [];
 
+function Newmember (name, position, id, office, email, github){
+    this.name = name;
+    this.position = position;
+    this.id = id;
+    this.office = office;
+    this.email = email;
+    this.github = github;
+    this.member = () => {
+        console.log([this.name])
+    }
+}
 
 const addEngineer = () => {
     inquirer
@@ -15,11 +27,11 @@ const addEngineer = () => {
                 name: 'name',
                 message: "What is this engineer's full name?",
             },
-            // {
-            //     type: 'input',
-            //     message: 'Record their identification number.',
-            //     name: 'id',
-            // },
+            {
+                type: 'input',
+                message: 'Record their identification number.',
+                name: 'id',
+            },
             // {
             //     type: 'input',
             //     message: 'Write their office number.',
@@ -36,41 +48,50 @@ const addEngineer = () => {
             //     name: 'github',
             // },
         ])
-        .then((engineerData) => {
-            const renEngDat = JSON.stringify(engineerData)
-            console.log(`Here's the information about the engineer:${renEngDat}`)
+        .then((data) => {
+            const engineerData = new newmember.Newmember(data.name, "Engineer", data.id, data.office, data.email, data.github)
+            console.log(JSON.stringify(engineerData))
+            //push information into the teamInfo array
             addNewOrExit()
         })
 }
 
-function addIntern() {
-    // //Todo, modify:
-    // type: 'input',
-    // name: 'name',
-    // message: "What is your team's manager's full name?",
-    // },
-    // {
-    //     type: 'input',
-    //     message: 'Record their identification number.',
-    //     name: 'id',
-    // },
-    // {
-    //     type: 'input',
-    //     message: 'Write their office number.',
-    //     name: 'office',
-    // },
-    // {
-    //     type: 'input',
-    //     message: 'Write their email.',
-    //     name: 'email',
-    // },
-    // {
-    //     type: 'input',
-    //     message: 'Write their identification number.',
-    //     name: 'github',
-    // },
-
-}
+const addIntern = () => {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: "What is this intern's full name?",
+            },
+            {
+                type: 'input',
+                message: 'Record their identification number.',
+                name: 'id',
+            },
+            // {
+            //     type: 'input',
+            //     message: 'Write their office number.',
+            //     name: 'office',
+            // },
+            // {
+            //     type: 'input',
+            //     message: 'Write their email.',
+            //     name: 'email',
+            // },
+            // {
+            //     type: 'input',
+            //     message: 'Write their identification number.',
+            //     name: 'github',
+            // },
+        ])
+        .then((data) => {
+            const internData = new newmember.Newmember(data.name, "Intern", data.id, data.office, data.email, data.github)
+            console.log(JSON.stringify(internData))
+            //push information into the teamInfo array
+            addNewOrExit()
+        })
+    }
 
 const addNewOrExit = () => {
     inquirer
@@ -98,7 +119,7 @@ const addNewOrExit = () => {
                 console.log("intern")
             }
         })
-    }
+}
 
 const init = () => {
     inquirer
@@ -108,11 +129,11 @@ const init = () => {
                 name: 'name',
                 message: "What is your team's manager's full name?",
             },
-            // {
-            //     type: 'input',
-            //     message: 'Record their identification number.',
-            //     name: 'id',
-            // },
+            {
+                type: 'input',
+                message: 'Record their identification number.',
+                name: 'id',
+            },
             // {
             //     type: 'input',
             //     message: 'Write their office number.',
@@ -135,7 +156,16 @@ const init = () => {
             const managerData = new newmember.Newmember(data.name, "Manager", data.id, data.office, data.email, data.github)
             console.log(data)
             addNewOrExit()
+            //push information into the teamInfo array
         })
-    }
+}
 
-    init();
+const buildTeam = () => {
+    //turn the teamInfo array into  
+}
+
+function writeToFile(fileName, data) {
+
+}
+
+init();

5a8fa71cea6236d8b9aa6c319977fa1ae550e5c3
Jay Ring-Sakabe <jrins@Jays-MacBook-Pro.local>
2022-08-23 17:43:00 -0600

    made video demonstration, completed readme

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -2,10 +2,10 @@ const Manager = require("../lib/Manager")
 
 describe ("Manager", () => {
     it("should gather the manager's name, id, email, and office number. ", () => {
-        const testManager = new Manager("John Doe", "54321","john@doe.com", "666, because it looks like the manager is Satan!")
+        const testManager = new Manager("John Doe", "54321", "666, because it looks like the manager is Satan!", "john@doe.com")
         expect(testManager.name).toEqual("John Doe")
         expect(testManager.id).toEqual("54321")
-        expect(testManager.email).toEqual("john@doe.com")
         expect(testManager.office).toEqual("666, because it looks like the manager is Satan!")
+        expect(testManager.email).toEqual("john@doe.com")
     })
 }) 
